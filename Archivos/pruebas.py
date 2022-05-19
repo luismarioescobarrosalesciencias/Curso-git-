6e061a813cdbe1c82ff8c576a8e3ab991200d9a1
Luis Mario <luismescobar@ciencias.unam.mx>
2022-05-19 13:44:19 -0500

    añadiendo archivos

---
+++ Archivos/pruebas.py
@@ -0,0 +1,19 @@
+import time
+
+f = [1, 2, 5, 4, 7, 5, 3,9,10,12,17,2,1]
+
+start = time.time()
+d = [i**2 for i in f if i % 2] #
+print('Segundo: ', time.time() - start)
+
+dic = {
+    'uno': 12,
+    'dos': 2.0,
+     3: 543,
+    'cuatro': 'ads',
+     4: 'asdf'
+    }
+
+keys = ['uno', 'dos', 'cuatro']
+new = {k: dic[k] for k in keys}
+print(new)
