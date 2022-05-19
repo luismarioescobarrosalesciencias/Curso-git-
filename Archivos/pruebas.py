9cfe0e2da34939e52350fffbfd7a4feacaaad0aa
Luis Mario <luismescobar@ciencias.unam.mx>
2022-05-19 16:25:04 -0500

    agregando funciones

--- Archivos/pruebas.py
+++ Archivos/pruebas.py
@@ -17,4 +17,53 @@ dic = {
 keys = ['uno', 'dos', 'cuatro']
 new = {k: dic[k] for k in keys}
 print(new)
-print("hola")
+
+
+def palindromo(cadenas):
+    for i, c in enumerate(cadenas):
+        cadena_min = c.lower().replace(' ', '')
+        if cadena_min == cadena_min[::-1]:
+            return i
+
+def conjunto(lista):
+    return set(lista)
+
+def duplicados(nums):
+    set1=set()
+    for i in range(0,len(nums)):
+        if nums[i] not in set1:
+            set1.add(nums[i])
+        else:
+            return False
+    return True
+
+
+def anagrama_valido(t,s):
+    if len(s)!=len(t):
+                return False
+    dic={}
+    for i in range(0,len(s)):
+        if s[i]  in dic:
+            dic[s[i]]+=1
+
+        else:
+            dic[s[i]]=1
+
+    for st  in t:
+        if st not in dic:
+            return False
+        else:
+            dic[st]-=1
+
+    for key in dic:
+        if dic[key]!=0:
+            return False
+    return True
+
+
+
+cadenas = ['Hola como estas',
+           'Lista de cadenas',
+           'Anita lava la tina',
+           'AJDSHFGDSHF']
+palindromo(cadenas)
